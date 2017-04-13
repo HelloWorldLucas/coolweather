726d58326087d8783f823a66bd5d187e53b6f891
Lucas <gong_lingxiao@163.com>
2017-04-13 18:51:33 +0800

    add sql table and settings'

---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,38 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Gonglx on 2017/4/13.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private int provinceId;
+    private String provinceName;
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
